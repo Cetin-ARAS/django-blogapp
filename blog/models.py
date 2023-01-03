addf316db9734b0eed6eec7c04fc72a38a9287f8
ÇETİN <cetinnnarasss@gmail.com>
2023-01-04 01:48:39 +0300

    models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        retun self.name
+
+class Blog(models.Model):
+    # CHOICES = (
+    #     ('p', 'Published'),
+    #     ('D', 'Draft')
+    # )
+    title = models.CharField(max_length=120)
+    content = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    # status = models.CharField(choices=CHOICES, default='d')
+    is_published = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def__str__(self):
+        return self.title
+    
+
+
