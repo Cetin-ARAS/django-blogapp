e977ca7976f5f32b16795a6a21748e0fd41bb290
ÇETİN <cetinnnarasss@gmail.com>
2023-01-04 05:50:06 +0300

    user

--- main/settings.py
+++ main/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # my apps
     "blog",
+    "user",
 
    # third party
    "rest_framework",
@@ -135,4 +136,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',                ]
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
